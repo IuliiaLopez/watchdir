02504fcc21e89b2019c1cd6784930be2e8b52ef6
JuliaOvcharenko <ovcharenko.iuliya12@gmail.com>
2019-12-15 12:29:43 +0200

    initial watchdir

--- watchdir.py
+++ watchdir.py
@@ -1,33 +1,53 @@
-from watchdog.observer import observer
+import os
+import sys
+import time
+
+# Extended version of https://pythonhosted.org/watchdog/quickstart.html#a-simple-example
 from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
-import os, json, time
-
-source_directory = "~/Downloads"
-target_directory = "~/Downloads/moved_files"
+# Directory we track, and target directory for the files
+track_directory = "/Users/julia/Downloads"
+target_directory = "/Users/julia/Downloads/moved_files"
 
 
 class DirectoryHandler(FileSystemEventHandler):
-    '''
+    """
     Event Handler for directory
-    '''
-    
+    """
+
     def on_modified(self, event):
-        '''
+        """
         On modified content within directory, change path within directory tree (the location of your file)
-        '''
-        for filename in os.listdir(directory):
-            current_location =  
-            new_location += "/" + filename
-            os.rename (src, new_target)
+        """
+        for filename in os.listdir(track_directory):
+            current_complete_filename = track_directory + "/" + filename
+            new_complete_filename = target_directory + "/" + filename
+            # odd thing to fix: OSError: [Errno 22] Invalid argument: '/Users/julia/Downloads/moved_files' -> '/Users/julia/Downloads/moved_files/moved_files'
+            os.rename(current_complete_filename, new_complete_filename)
 
 
-event_handler = DirectoryHandler()
-observer = Observer()
-observer.shedule(event_handler, source_directory, recursive=True)
+def main():
+    event_handler = DirectoryHandler()
 
-try:
-    while True:
-        # scan every ten seconds the directory for changes
-        sleep(10)
- 
+    observer = Observer()
+    observer.schedule(event_handler, track_directory, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(5)
+            print("watching for another 5 sec")
+    except (KeyboardInterrupt, SystemExit):
+        print("Stopped watching your directory")
+        observer.stop()
+    observer.join()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
